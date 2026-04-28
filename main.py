7d60cb4cf6d5bbc1d45cf66d8c071ac7ba77f1e0
msbelajar <126963165+msbelajar@users.noreply.github.com>
2026-04-28 10:35:42 +0800

    update https

--- main.py
+++ main.py
@@ -2,20 +2,18 @@ from fastapi import Depends, FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
-from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
 from routers import admin, dosen, test, auth, root
 from security import verify_cookie_token
 
 app = FastAPI(title="App Seminar")
 
-# Add middleware to handle HTTPS from reverse proxy
-app.add_middleware(
-    TrustedHostMiddleware, allowed_hosts=["sisma.fmipa.uho.ac.id"]
-)
+# Better than custom HTTPS middleware
+app.add_middleware(ProxyHeadersMiddleware)
+
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Exception handler for 401 Unauthorized - redirect to login
 @app.exception_handler(HTTPException)
 async def http_exception_handler(request, exc):
     if exc.status_code == 401:
